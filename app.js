b2e49b4ec64ddc8cdec2494c70d756f0a9c6f5cd
kelvv <kelvv@outlook.com>
2016-06-14 01:31:45 +0800

    last share

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
-var koa = require('koa');
-var wechat = require('co-wechat');
-var app = koa();
+require('./lib/utils/stringExt');
+const koa = require('koa');
+const wechat = require('co-wechat');
+const app = koa();
+const fs 		= require('fs-extra');
+fs.mkdirpSync(process.cwd() + '/logs');
+const applogger = require('./lib/utils/log').get('app');
+const movieHandler = require('./lib/middleware/movieHandler');
+const env = require('./lib/config/env');
 
-app.use(wechat('kelvvwechattoken').middleware(function *() {
+/*app.use(wechat('kelvvwechattoken').middleware(function *() {
 	var message = this.weixin;
-	this.body = 'kelvv and nikki  https://pan.baidu.com/s/1i5IvWl3';
-}));
+	var resulturl = '';
+	
+	this.body = '已为您找到观看地址：'+resulturl;
+}));*/
 
-app.listen(443);
+app.use(function *(next) {
+	if(this.originalUrl==='/favicon.ico'){
+		return;
+	}
+	yield next;
+})
+
+app.use(function *(next){
+	var searchName = '爱丽丝梦游仙境2';
+	this.searchName = searchName;
+	let context = this;
+	this.send = (body) => {
+		context.status = 200;
+		context.body = body;
+	}
+	yield next;
+});
+
+app.use(movieHandler());
+
+app.listen(env.PORT || 9302);
+applogger.info('-----------app is listening in 9302------------');
